3d4d3e011253873f346dd374d45fe9a10f1ca5c8
Florent LOTTIN <flottin@gmail.com>
2018-07-27 13:33:26 +0200

    atom-build for easier compilation

--- src/main/java/in/karatube/App.java
+++ src/main/java/in/karatube/App.java
@@ -1,7 +1,7 @@
 package in.karatube;
 import org.apache.commons.cli.*;
 import redis.clients.jedis.*;
-import de.vandermeer.asciitable.*;
+
 import java.io.Console;
 import java.util.Random;
 import java.lang.Math;
@@ -14,43 +14,44 @@ public class App
 {
     public static void main( String[] args )
     {
-        Console console = System.console();
-    if (console != null) {
-        //console.format("console exists!");
 
-
-    } else {
-        Fenetre f = new Fenetre();
-    }
 
         Options options = new Options();
         options.addOption("b", "bars", true, "display horizontal bars : the parameter is a number of bars requested");
         options.addOption("c", "color", true, "color");
         options.addOption("m", "msg", true, "colorized msg");
         options.addOption("d", "dry-run", true, "make a dry-run");
+        options.addOption("h", "help", false, "display help");
+
+        options.addOption("f", "figure", true, "shape mv ");
+        options.addOption("r", "random", true, "randomise");
         options.addOption("s", "size", true, "shape size");
-        options.addOption("f", "figure", true, "shape ");
         options.addOption("t", "table", true, "table ");
         options.addOption("u", "utf8", true, "utf8");
         options.addOption("w", "windows", true, "windows");
 
-
         String header = "\nGenerate many shapes!\n\n";
         String footer = "\nPlease report issues at https://github.com/flottin/shape/issues";
 
-        HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("java -jar shape.jar", header, options, footer, true);
-        Random r = new Random();
-        System.out.println(Math.floor(r.nextFloat() * 10));
+      try {
         CommandLineParser parser = new DefaultParser();
-	    try {
             CommandLine cmd = parser.parse( options, args);
+
+            if (null == args || cmd.hasOption("h") == true ){
+              HelpFormatter formatter = new HelpFormatter();
+              formatter.printHelp("java -jar shape.jar", header, options, footer, true);
+
+            }
 
             if(cmd.hasOption("b")) {
                 String number = cmd.getOptionValue("b");
                 int size = Integer.parseInt(number);
                 Shape shape = new Shape();
                 shape.bars(size);
+            }
+
+            if(cmd.hasOption("c") && cmd.hasOption("m")) {
+                ConsoleColors.display(cmd.getOptionValue("m"), cmd.getOptionValue("c"));
             }
 
             if(cmd.hasOption("d")) {
@@ -62,6 +63,16 @@ public class App
                 Shape shape = new Shape();
                 shape.shape(name);
             }
+
+            if(cmd.hasOption("r")) {
+              Random r = new Random();
+              String number = cmd.getOptionValue("r");
+              int size = Integer.parseInt(number);
+
+              System.out.println((int)Math.floor(r.nextFloat() * size));
+
+            }
+
             if(cmd.hasOption("s")) {
                 String number = cmd.getOptionValue("s");
                 int size = Integer.parseInt(number);
@@ -69,35 +80,32 @@ public class App
                 shape.square(size);
             }
 
-            if(cmd.hasOption("t")) {
-                AsciiTable at = new AsciiTable();
 
-                at.addRule();
-                at.addRow("row 1 col 1", "row 1 col 2");
-                at.addRule();
-                at.addRow("row 2 col 1", "row 2 col 2");
-                at.addRule();
 
-                String rend = at.render();
-
-                System.out.println(rend);
-            }
-
-            if(cmd.hasOption("c") && cmd.hasOption("m")) {
-                ConsoleColors.display(cmd.getOptionValue("m"), cmd.getOptionValue("c"));
-            }
             if(cmd.hasOption("u")) {
                 System.out.println("\u272A");
             }
+
+
             if(cmd.hasOption("w")) {
-        Fenetre w = new Fenetre();
+              if ("true" == cmd.getOptionValue("w")){
+                Fenetre w = new Fenetre();
+              }
+            }else{
+              Console console = System.console();
+              if (console != null) {
+
+              } else {
+                  Fenetre f = new Fenetre();
+              }
             }
         } catch (Exception e) {
 
         }
 
 
-    }
+      }
+
 
     /**
     // System.out.println(implode(", ", new String[] { "ab", " ", "abs" }));
