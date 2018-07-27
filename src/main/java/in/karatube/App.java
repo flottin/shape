6ff8b64a832ee9de8367958b7af90b3d60244c01
Florent LOTTIN <flottin@gmail.com>
2018-07-27 15:45:34 +0200

    making an implode

--- src/main/java/in/karatube/App.java
+++ src/main/java/in/karatube/App.java
@@ -3,6 +3,7 @@ import org.apache.commons.cli.*;
 import java.io.Console;
 import java.util.Random;
 import java.lang.Math;
+import java.lang.StringBuilder;
 
 /**
  * making shape
@@ -93,21 +94,23 @@ public class App
         } catch (Exception e) {
 
         }
+
+        System.out.println(implode("~", new String[] { "ab", " ", "abs" }));
+
       }
 
     /**
-    // System.out.println(implode(", ", new String[] { "ab", " ", "abs" }));
+    *  System.out.println(implode(", ", new String[] { "ab", " ", "abs" }));
     */
     public static String implode(String separator, String... data) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < data.length - 1; i++) {
-        //data.length - 1 => to not add separator at the end
-            if (!data[i].matches(" *")) {//empty string are ""; " "; "  "; and so on
-                sb.append(data[i]);
-                sb.append(separator);
-            }
+        for (int i = 0; i < data.length; i++) {
+          sb.append(data[i]);
+          if (i < data.length - 1){
+            sb.append(separator);
+          }
+
         }
-        sb.append(data[data.length - 1].trim());
         return sb.toString();
     }
 }
